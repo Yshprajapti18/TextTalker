53921064d5a80d769efb75e7c6c363036fdedf2e
Yash Prajapati <ysh182002@gmail.com>
2023-11-04 02:36:16 +0530

    made a chatbot with help of hugchat api

--- Chatbot/chatbot/chatbot_django/views.py
+++ Chatbot/chatbot/chatbot_django/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+from hugchat import hugchat
+from hugchat.login import Login
+
+
+
+
+
+   
+
+
+def chatbot(request):
+    sign = Login('jicsocialmedia1234@gmail.com', 'QezS8>=^rfR~,"3')
+    cookies = sign.login()
+
+    cookie_path_dir = "./cookies_snapshot"
+    sign.saveCookiesToDir(cookie_path_dir)
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = chatbot.query(message, web_search=True)
+        
+        return  JsonResponse({'message' : message , 'response' : str(response)})
+    return render(request,'chatbot.html')
