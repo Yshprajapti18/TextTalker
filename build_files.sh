cce5cb9f31d304903138a3b6546bdebd6f13e1ac
Yash Prajapati <ysh182002@gmail.com>
2023-11-23 23:41:56 +0530

    updated build files

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo = "BUILD START"
 
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 cd Chatbot/chatbot
 
-python3.10  manage.py collectstatic --noinput --clear
+python3  manage.py collectstatic --noinput --clear
 
 echo = "BUILD END"
