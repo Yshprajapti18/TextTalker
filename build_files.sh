054f8e34e93c9e177bbc58501b52227946c57c47
Yash Prajapati <ysh182002@gmail.com>
2024-02-07 17:48:09 +0530

    some changes

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,6 @@ echo = "BUILD START"
 
 python3 -m pip install -r requirements.txt
 
-python3  manage.py collectstatic --noinput --clear
+python3  manage.py collectstatic --noinput 
 
 echo = "BUILD END"
